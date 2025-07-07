4f8eab08045d025ff6f59e0b187d4ada9076cc6d
athanase25537 <andriamasyathanase.nazy@gmail.com>
2025-07-07 22:30:52 +0300

    update: final version

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { Header } from "./components/header/header";
 import { Main } from "./components/main/main";
-import { Card } from './components/card/card';
 import { Myservices } from "./components/myservices/myservices";
-import { CardResume } from "./components/card-resume/card-resume";
 import { SchoolResume } from "./components/school-resume/school-resume";
 import { Portfolio } from "./components/portfolio/portfolio";
 import { Contact } from "./components/contact/contact";
@@ -11,7 +9,7 @@ import { Techno } from "./components/techno/techno";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Main, Card, Myservices, CardResume, SchoolResume, Portfolio, Contact, Techno],
+  imports: [Header, Main, Myservices, SchoolResume, Portfolio, Contact, Techno],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
